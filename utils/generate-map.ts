a0802ed9accb379bbf35abb8aed6fe4f5ffbcf35
yjgaia <yjdev88@gmail.com>
2024-07-12 12:53:30 +0900

    objects -> objects / map objects

--- utils/generate-map.ts
+++ utils/generate-map.ts
@@ -29,7 +29,7 @@ enum TerrainDirection {
   FillBottomRight = "fill-bottom-right",
 }
 
-interface TilemapData {
+interface MapData {
   terrains: {
     [id: string]: {
       [direction: string]: {
@@ -39,26 +39,28 @@ interface TilemapData {
       }[];
     };
   };
-  terrainMap: { [cord: string]: string }; // { row, col } -> terrainId
   objects: {
-    x: number;
-    y: number;
-    zIndex: number;
-    spritesheet: string;
-    frame: string;
-  }[];
+    [id: string]: {
+      spritesheet: string;
+      frame: string;
+      zIndex: number;
+    };
+  };
+  terrainMap: { [cord: string]: string }; // { row, col } -> terrainId
+  mapObjects: { x: number; y: number; object: string }[];
 }
 
-const mapData = fs.readFileSync("./data/map-tiles.txt", "utf-8");
+const mapTiles = fs.readFileSync("./data/map-tiles.txt", "utf-8");
 
-const tilemapData: TilemapData = {
+const mapData: MapData = {
   terrains: {},
+  objects: {},
   terrainMap: {},
-  objects: [],
+  mapObjects: [],
 };
 
 for (const [tileId, tileData] of Object.entries(tilesData)) {
-  tilemapData.terrains[`terrain-${tileId}`] = {
+  mapData.terrains[`terrain-${tileId}`] = {
     [TerrainDirection.TopLeft]: tileData.leftTop.parts[0].frames.map((frame) =>
       (keyToSpritesheet as any)[frame]
     ),
@@ -126,9 +128,9 @@ for (const [tileId, tileData] of Object.entries(tilesData)) {
   };
 }
 
-for (let i = 0; i < mapData.length; i += 14) {
-  const tileId = mapData[i];
-  tilemapData
+for (let i = 0; i < mapTiles.length; i += 14) {
+  const tileId = mapTiles[i];
+  mapData
     .terrainMap[`${Math.floor(i / (100 * 14)) - 50},${i / 14 % 100 - 50}`] =
       `terrain-${tileId}`;
 }
@@ -136,15 +138,22 @@ for (let i = 0; i < mapData.length; i += 14) {
 for (const mapObject of mapObjects as any) {
   const key = (objectsData as any)[mapObject.objectIndex][mapObject.state]
     .parts[0][mapObject.direction].frames[mapObject.kind];
-  tilemapData.objects.push({
+  const spritesheet = (keyToSpritesheet as any)[key];
+
+  mapData.objects[spritesheet.frame] = {
+    spritesheet: spritesheet.spritesheet,
+    frame: spritesheet.frame,
+    zIndex: 5,
+  };
+
+  mapData.mapObjects.push({
+    object: spritesheet.frame,
     x: mapObject.x,
     y: mapObject.y,
-    ...(keyToSpritesheet as any)[key],
-    zIndex: 5,
   });
 }
 
 fs.writeFileSync(
   "../public/assets/optimized-tiles/map.json",
-  JSON.stringify(tilemapData, null, 2),
+  JSON.stringify(mapData, null, 2),
 );
