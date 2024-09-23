f44dc2a0cf27912e990123ee2fc221c4c1ff8a39
yjgaia <yjdev88@gmail.com>
2024-09-23 16:28:05 +0900

    Add Supabase project configuration properties

--- game/dev.ts
+++ game/dev.ts
@@ -2,4 +2,7 @@ import init from "./init.js";
 
 await init({
   isDevMovde: true,
+  supabaseUrl: "https://itqgxihqmbuqpbuduktk.supabase.co",
+  supabaseKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0cWd4aWhxbWJ1cXBidWR1a3RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwNzM4ODgsImV4cCI6MjA0MjY0OTg4OH0.ZXKwLDmBweWHCMEFVRKcdr9n2M6sD9wjTPryUIgnrQk",
 });
--- game/prod.ts
+++ game/prod.ts
@@ -2,4 +2,6 @@ import init from "./init.js";
 
 await init({
   isDevMovde: false,
+  supabaseUrl: "",
+  supabaseKey: "",
 });
