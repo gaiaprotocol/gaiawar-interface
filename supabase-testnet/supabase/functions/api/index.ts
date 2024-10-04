954baccb3aa5b2847194af120b813e913e284d76
yjgaia <yjdev88@gmail.com>
2024-10-04 15:15:40 +0900

    Fix import paths in index.ts

--- supabase-testnet/supabase/functions/api/index.ts
+++ supabase-testnet/supabase/functions/api/index.ts
@@ -1,6 +1,6 @@
-import { startServer } from "https://raw.githubusercontent.com/yjgaia/deno-module//main/api.ts";
+import { startServer } from "https://raw.githubusercontent.com/yjgaia/deno-module/main/api.ts";
 import { serveWalletApi } from "https://raw.githubusercontent.com/yjgaia/wallet-login-module/main/deno/wallet.ts";
-import { serveContractApi } from "https://raw.githubusercontent.com/yjgaia/contract-module//main/deno/contract.ts";
+import { serveContractApi } from "https://raw.githubusercontent.com/yjgaia/contract-module/main/deno/contract.ts";
 
 startServer(
   serveWalletApi,
