00fa8bf58f75050ac725ca3b18242330286e25a4
yjgaia <yjdev88@gmail.com>
2024-12-15 21:38:12 +0900

    feat: Refactor TileHoverOverlay to use TargetingShape for dynamic stroke width based on camera scale

--- game/world/tile-overlays/TileHoverOverlay.ts
+++ game/world/tile-overlays/TileHoverOverlay.ts
@@ -1,12 +1,72 @@
-import { Sprite } from "@gaiaengine/2d";
+import { ShapeNode, Sprite } from "@gaiaengine/2d";
+import { StrokeInput } from "../../../../pixi.js/lib/index.js";
 import BuildingManager from "../../data/building/BuildingManager.js";
+import GameConfig from "../../GameConfig.js";
 import TileBase from "../TileBase.js";
+
+class TargetingShape extends ShapeNode {
+  private _stroke: StrokeInput | undefined;
+  private currentScale: number | undefined;
+
+  constructor() {
+    super(0, 0);
+  }
+
+  protected draw(): void {
+    this.container.clear();
+
+    const tileSize = GameConfig.tileSize;
+    const half = tileSize / 2;
+
+    this.container.moveTo(-half, -half + tileSize / 3)
+      .lineTo(-half, -half)
+      .lineTo(-half + tileSize / 3, -half);
+
+    this.container.moveTo(tileSize - tileSize / 3 - half, -half)
+      .lineTo(tileSize - half, -half)
+      .lineTo(tileSize - half, -half + tileSize / 3);
+
+    this.container.moveTo(tileSize - half, tileSize - tileSize / 3 - half)
+      .lineTo(tileSize - half, tileSize - half)
+      .lineTo(tileSize - tileSize / 3 - half, tileSize - half);
+
+    this.container.moveTo(-half + tileSize / 3, tileSize - half)
+      .lineTo(-half, tileSize - half)
+      .lineTo(-half, tileSize - tileSize / 3 - half);
+
+    this.container.stroke(this._stroke);
+  }
+
+  public set stroke(stroke: StrokeInput | undefined) {
+    this._stroke = stroke;
+    this.draw();
+  }
+
+  public get stroke() {
+    return this._stroke;
+  }
+
+  protected update(deltaTime: number): void {
+    if (this.screen) {
+      const newScale = this.screen.camera.scale;
+      if (newScale !== this.currentScale) {
+        this.currentScale = newScale;
+
+        this.stroke = {
+          width: 1 / this.currentScale,
+          color: 0x48D1CC,
+        };
+      }
+    }
+    super.update(deltaTime);
+  }
+}
 
 class TileHoverOverlay extends TileBase {
   private buildingPreview: Sprite | undefined;
 
   constructor() {
-    super(-999999, -999999, new Sprite(0, 0, "/assets/tile/hover.png"));
+    super(-999999, -999999, new TargetingShape());
   }
 
   public async setBuildingPreview(buildingId: number) {
