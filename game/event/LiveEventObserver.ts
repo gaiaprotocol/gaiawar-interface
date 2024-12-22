21df9535dbe80606f964a4850d9155f3d1e5a765
yjgaia <yjdev88@gmail.com>
2024-12-22 14:39:05 +0900

    feat: Simplify LiveEventObserver install method by removing channel property and adding test parameter to subscribeToPresence

--- game/event/LiveEventObserver.ts
+++ game/event/LiveEventObserver.ts
@@ -1,15 +1,14 @@
-import { RealtimeChannel } from "@supabase/supabase-js";
 import GameConfig from "../core/GameConfig.js";
 
 class LiveEventObserver {
-  private channel!: RealtimeChannel;
-
   public install() {
-    this.channel = GameConfig.supabaseConnector.subscribeToPresence({
+    GameConfig.supabaseConnector.subscribeToPresence({
       channel: "live-events",
       onSync: (state) => {
         console.log(state);
       },
+    }, {
+      test: 123,
     });
   }
 }
