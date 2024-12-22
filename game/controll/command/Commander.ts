4a5e94b29e38410a2f386cd55481115a9c7197c3
yjgaia <yjdev88@gmail.com>
2024-12-22 23:38:04 +0900

    feat: Enhance Commander class with command waiting functionality for building and unit actions

--- game/controll/command/Commander.ts
+++ game/controll/command/Commander.ts
@@ -1,6 +1,29 @@
 import { Coordinates } from "@gaiaengine/2d";
+import { UnitQuantity } from "../../data/tile/TileData.js";
 
 class Commander {
+  private waitingCommand:
+    | "construct"
+    | "move"
+    | "move-and-attack"
+    | "ranged-attack"
+    | undefined;
+  private waitingBuilding: number | undefined;
+  private waitingUnits: UnitQuantity[] | undefined;
+
+  public waitForBuildingCommand(command: "construct", building: number) {
+    this.waitingCommand = command;
+    this.waitingBuilding = building;
+  }
+
+  public waitForUnitCommand(
+    command: "move" | "move-and-attack" | "ranged-attack",
+    units: UnitQuantity[],
+  ) {
+    this.waitingCommand = command;
+    this.waitingUnits = units;
+  }
+
   public selectTile(coord: Coordinates) {
     //TODO:
   }
