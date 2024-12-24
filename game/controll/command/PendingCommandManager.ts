0403297b7a4179a931c1d86fa2b6e8248e618a72
yjgaia <yjdev88@gmail.com>
2024-12-24 14:03:50 +0900

    feat: Enhance PendingCommandManager with key generation for pending commands and update sync logging

--- game/controll/command/PendingCommandManager.ts
+++ game/controll/command/PendingCommandManager.ts
@@ -4,26 +4,38 @@ import PendingCommand from "./PendingCommand.js";
 
 class PendingCommandManager {
   private channel!: RealtimeChannel;
-  private pendingCommands = {};
+  private pendingCommands: Record<string, PendingCommand> = {};
 
   public init() {
     this.channel = GaiaWarConfig.supabaseConnector.subscribeToPresence(
       "pending-commands",
       {
         onSync: (state) => {
-          console.log("sync", state);
+          console.log("sync", JSON.stringify(state));
         },
       },
       this.pendingCommands,
     );
   }
 
+  private makeKey(pendingCommand: PendingCommand) {
+    let key = `${pendingCommand.type}`;
+    if (pendingCommand.from) {
+      key += `-${pendingCommand.from.x},${pendingCommand.from.y}`;
+    } else if (pendingCommand.to) {
+      key += `-${pendingCommand.to.x},${pendingCommand.to.y}`;
+    }
+    return key;
+  }
+
   public addPendingCommand(pendingCommand: PendingCommand) {
-    //TODO:
+    this.pendingCommands[this.makeKey(pendingCommand)] = pendingCommand;
+    this.channel.track(this.pendingCommands);
   }
 
   public removePendingCommand(pendingCommand: PendingCommand) {
-    //TODO:
+    delete this.pendingCommands[this.makeKey(pendingCommand)];
+    this.channel.track(this.pendingCommands);
   }
 }
 
