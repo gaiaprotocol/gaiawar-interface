4b7b88e704047892a90caf4305b7d0c419deb72d
yjgaia <yjdev88@gmail.com>
2024-12-24 14:46:15 +0900

    feat: Add new flag icons for construction, training, movement, attack, ranged attack, and loot collection; update Flag class to utilize new icons

--- game/game-objects/flag/Flag.ts
+++ game/game-objects/flag/Flag.ts
@@ -9,10 +9,24 @@ export default class Flag extends GameObject {
       -19,
       64,
     );
-    if (type === PendingCommandType.CONSTRUCT) {
-      this.append(new Sprite(1, -25, "/assets/flags/icons/constructing.png"));
-    } else if (type === PendingCommandType.TRAIN) {
-      this.append(new Sprite(1, -25, "/assets/flags/icons/training.png"));
+    if (
+      type === PendingCommandType.CONSTRUCT ||
+      type === PendingCommandType.UPGRADE_BUILDING
+    ) {
+      this.append(new Sprite(1, -25, "/assets/flags/icons/construct.png"));
+    } else if (
+      type === PendingCommandType.TRAIN ||
+      type === PendingCommandType.UPGRADE_UNIT
+    ) {
+      this.append(new Sprite(1, -25, "/assets/flags/icons/train.png"));
+    } else if (type === PendingCommandType.MOVE) {
+      this.append(new Sprite(1, -25, "/assets/flags/icons/move.png"));
+    } else if (type === PendingCommandType.MOVE_AND_ATTACK) {
+      this.append(new Sprite(1, -25, "/assets/flags/icons/attack.png"));
+    } else if (type === PendingCommandType.RANGED_ATTACK) {
+      this.append(new Sprite(1, -25, "/assets/flags/icons/ranged-attack.png"));
+    } else if (type === PendingCommandType.COLLECT_LOOT) {
+      this.append(new Sprite(1, -25, "/assets/flags/icons/collect.png"));
     }
   }
 }
