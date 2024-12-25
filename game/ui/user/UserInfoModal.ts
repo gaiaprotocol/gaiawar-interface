555453b35cf7796fc7270f13bb1b5244cfeab39b
yjgaia <yjdev88@gmail.com>
2024-12-25 18:14:23 +0900

    feat: Implement UserInfoModal; add persona loading functionality, display user info, and create persona option for logged-in users

--- game/ui/user/UserInfoModal.ts
+++ game/ui/user/UserInfoModal.ts
@@ -1,7 +1,51 @@
-import { StructuredModal } from "@common-module/app-components";
+import { el } from "@common-module/app";
+import {
+  AppCompConfig,
+  Button,
+  ButtonType,
+  StructuredModal,
+} from "@common-module/app-components";
+import { WalletLoginManager } from "@common-module/wallet-login";
+import { PersonaDisplay, PersonaRepository } from "gaiaprotocol";
 
 export default class UserInfoModal extends StructuredModal {
   constructor(address: string) {
     super(".user-info-modal");
+    this.appendToHeader(el("h2", "User Info"));
+    this.loadPersona(address);
+  }
+
+  private async loadPersona(walletAddress: string) {
+    const loginUser = WalletLoginManager.getLoggedInAddress();
+
+    const loadingSpinner = new AppCompConfig.LoadingSpinner();
+    this.appendToMain(loadingSpinner);
+
+    const persona = await PersonaRepository.fetchPersona(walletAddress);
+
+    loadingSpinner.remove();
+
+    if (!persona) {
+      this.appendToMain(el(".no-persona", "No persona found"));
+      if (loginUser === walletAddress) {
+        this.appendToFooter(
+          new Button({
+            type: ButtonType.Contained,
+            title: "Create Persona",
+            onClick: () =>
+              window.open("https://personas.gaia.cc/onboarding", "_blank"),
+          }),
+        );
+      }
+    } else {
+      this.appendToMain(
+        new PersonaDisplay({
+          persona,
+          showEditButton: persona.wallet_address === loginUser,
+          onEditClick: () =>
+            window.open("https://personas.gaia.cc/edit-persona", "_blank"),
+        }),
+      );
+    }
   }
 }
